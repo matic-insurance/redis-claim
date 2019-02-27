40adac0dc31ab2fe55f48faa05e4be574e5d22ac
Volodymyr Mykhailyk <volodymyr.mykhailyk@gmail.com>
2019-02-27 15:01:08 +0200

    Add dummy interface

--- spec/redis/claim_spec.rb
+++ spec/redis/claim_spec.rb
@@ -2,4 +2,8 @@ RSpec.describe Redis::Claim do
   it "has a version number" do
     expect(Redis::Claim::VERSION).not_to be nil
   end
+
+  it 'works out of the box' do
+    expect { Redis::Claim.verify }.to_not raise_error
+  end
 end
